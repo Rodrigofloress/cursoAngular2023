cfc200e3dd0dc4b0cd8d2bb2b286527fec9275d4
Rodrigo Flores <floresrodri223@gmail.com>
2023-10-10 14:35:41 -0300

    Actualizacion y solucion de appmodule bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/routes/home/home.component';
+import { IngresarComponent } from './components/routes/ingresar/ingresar.component';
+import { SeriesComponent } from './components/routes/series/series.component';
+import { MoviesComponent } from './components/routes/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  },
+  {
+    path: 'ingresar',
+    component: IngresarComponent
+  },
+  {
+    path: 'series',
+    component: SeriesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'desafio1'`, () => {
+  it(`should have as title 'PelisUp!`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('desafio1');
+    expect(app.title).toEqual('PelisUp!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('desafio1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PelisUp! app is running!');
   });
 });
